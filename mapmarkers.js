e5371cf7bbbe0a2837c399b159d5ca80e17e2eec
whoisaplex <32065397+whoisaplex@users.noreply.github.com>
2018-01-23 17:04:27 +0100

    position set to data

--- mapmarkers.js
+++ mapmarkers.js
@@ -4,6 +4,7 @@ let socket;
 function getMarkers(){
   socket = io('http://localhost:3000');
   socket.on('sendData', (data) => {
+    positions = data;
     renderQuestMarkers(data);
     renderQuestItemList();
   });
