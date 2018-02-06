7666a0f308dc00ec924fbaa89008b867f5160fe0
liolios <simon.liolios@hyperisland.se>
2018-02-06 16:11:01 +0100

    add game css files

--- PHP/resources/views/game.blade.php
+++ PHP/resources/views/game.blade.php
@@ -85,5 +85,6 @@
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA-asKbHoJKTtGt8rWJ35ebHT3j7sVKUBA"></script>
     <script type="module" src="js/game.js"></script>
     <script type="text/javascript" src="js/games/js-game-colormatch.js"></script>
+        
 </body>
 </html>
