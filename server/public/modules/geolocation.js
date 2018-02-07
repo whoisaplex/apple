a98640f29d7171d25ad594288a9542ec72cc22c0
kaliber <robbineriksson@gmail.com>
2018-02-07 16:58:26 +0100

    removed the public folder from Node

--- server/public/modules/geolocation.js
+++
@@ -1,9 +0,0 @@
-// Inits geolocation
-export default function initGeolocation(user, map){
-    navigator.geolocation.watchPosition(onUpdatePos, onPosError);
-
-    function onUpdatePos(pos){
-        user.upDateCoords({lat: pos.coords.latitude, lng: pos.coords.longitude}, map.googleMap);
-    }
-    function onPosError(err){ console.log('Pos update error:', err) }
-}
